7346b7a381b6d99fdab8db4f1bcdbfe2902d570f
Revature <rqlmoura@yahoo.com>
2022-03-31 21:38:34 -0700

    Lockers

---
+++ LockersChallenge.java
@@ -0,0 +1,28 @@
+
+public class LockersChallenge {
+	
+	
+	public  int lockers(int lockersNumbers){
+		
+		
+        boolean[] drs = new boolean[lockersNumbers];
+
+        for (int i = 1; i <= lockersNumbers; i += 1){
+            for (int locker = i - 1; locker < lockersNumbers; locker += i){
+                drs[locker] = !drs[locker];
+            }
+        }
+
+        int numberOfDoors = 0;
+        for(boolean isOpen: doors) {
+            if (isOpen) {
+            	numberOfDoors++;
+            }
+        }
+
+        return numberOfDoors;
+    }
+	
+	
+
+}
